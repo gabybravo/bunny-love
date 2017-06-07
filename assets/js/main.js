fd0b5934290abbe78e884b764f29f8367c7f3f90
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-06-06 23:03:49 -0400

    JS listo y Media CSS realizado con los estilos para el responsive.

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById("menu-icono");
+
+menu.addEventListener("click", function() {
+	var nav = document.getElementById("nav-menu-js");
+	nav.classList.toggle("hide");
+
+
+})
